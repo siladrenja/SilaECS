3235c8a563d84946174356ac83f29341fe8bd0a2
matej drenjančević <matej.drenjancevic@gmail.com>
2024-06-13 21:14:07 +0200

    changed testing script

--- SilaECS/Source.cpp
+++ SilaECS/Source.cpp
@@ -7,19 +7,19 @@ constexpr size_t iterations = 1E9;
 
 class Count : public ECS::component {
 public:
-	void Run() {
-		++count;
-	}
 
-	static size_t count;
+	Count(int c) : c(c) {}
+
+	
+
+	int c;
 };
 
-size_t Count::count = 0;
-
+size_t sum = 0;
 struct Functor {
 	template <class T>
 	static void Invoke(T& print) {
-		print.Run();
+		sum += print.c;
 	}
 };
 
@@ -28,7 +28,7 @@ int main() {
 	auto t1 = std::chrono::steady_clock::now();
 	ECS::Prefab<Count> prefab;
 	for (size_t i = 0; i < iterations; i++) {
-		prefab.AddComponent(Count());
+		prefab.AddComponent(Count(i));
 	}
 	auto t2 = std::chrono::steady_clock::now();
 	ECS::World<Count> world;
@@ -37,7 +37,7 @@ int main() {
 	auto t3 = std::chrono::steady_clock::now();
 	world.ApplyToAll<Functor>();
 	auto t4 = std::chrono::steady_clock::now();
-	std::cout << Count::count << std::endl;
+	std::cout << sum << std::endl;
 	std::cout << "Prefab Creation: " << ((double)(t2 - t1).count() / 1E9) << "s\n" << 
 		"Entity creation: " << ((double)(t3 - t2).count() / 1E9) << "s\n" << 
 		"Iteration: " << (t4-t3).count() << "ns\n" << 
